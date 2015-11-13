e4037505417f454d14127d72673b6e0efc553274
Kurt Lippert <kurt.lippert@peoplefluent.com>
2015-11-13 13:29:54 -0500

    Removed unnecessary controller actions / views

--- MvcSandbox/Controllers/HomeController.cs
+++ MvcSandbox/Controllers/HomeController.cs
@@ -1,25 +1,11 @@
-﻿using System.Web.Mvc;
-
-namespace MvcSandbox.Controllers
+﻿namespace MvcSandbox.Controllers
 {
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
